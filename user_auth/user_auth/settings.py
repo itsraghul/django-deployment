74b62573d6bc4a9c717b5172bbe8af16c12d2b2c
Raghul S <58110802+itsraghul@users.noreply.github.com>
2021-10-01 16:24:46 +0530

    Added allowed host

--- user_auth/user_auth/settings.py
+++ user_auth/user_auth/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-a&dgjq@!ne^&ykvkrc375b#hxu5blu$zpcn#m9n26=)peygp8l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['raghul2521.pythonanywhere.com']
 
 
 # Application definition
